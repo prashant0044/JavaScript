6d3b15b3e4586d936ead43aff17fb57a10bcb1e9
Prashant Rana <ranaprashant282@gmail.com>
2025-08-19 12:03:35 +0530

    Nine Commit

---
+++ httprequest.js
@@ -0,0 +1,11 @@
+// Making a HTTP request
+
+fetch('https://jsonplaceholder.typicode.com/posts').then((response)=>{
+    return response.json().then((posts)=>{
+        console.log("Posts fetched successfully",posts);
+    });
+    
+}).catch((error)=>{
+    console.log("Error fetching posts",error); 
+});
+
